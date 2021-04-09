004d4de464e81e061505592ad059682ae87f40e8
Yos Riady <yosriady@gmail.com>
2021-04-09 17:58:46 +0800

    Clean up hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,28 +1,15 @@
+dotenvConfig({ path: resolve(__dirname, "./.env") });
 import "@nomiclabs/hardhat-waffle";
 import "hardhat-typechain";
 import "hardhat-deploy";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
-
 import "./tasks/accounts";
 import "./tasks/clean";
 
 import { resolve } from "path";
-
 import { config as dotenvConfig } from "dotenv";
-import { NetworkUserConfig, HardhatUserConfig } from "hardhat/types";
-
-dotenvConfig({ path: resolve(__dirname, "./.env") });
-
-const chainIds = {
-  ganache: 1337,
-  goerli: 5,
-  hardhat: 31337,
-  kovan: 42,
-  mainnet: 1,
-  rinkeby: 4,
-  ropsten: 3,
-};
+import { HardhatUserConfig } from "hardhat/types";
 
 // Ensure that we have all the environment variables we need.
 let mnemonic: string;
@@ -31,6 +18,9 @@ if (!process.env.MNEMONIC) {
 } else {
   mnemonic = process.env.MNEMONIC;
 }
+const accounts = {
+  mnemonic,
+};
 
 let infuraApiKey: string;
 if (!process.env.INFURA_API_KEY) {
@@ -39,33 +29,18 @@ if (!process.env.INFURA_API_KEY) {
   infuraApiKey = process.env.INFURA_API_KEY;
 }
 
-function createTestnetConfig(network: keyof typeof chainIds): NetworkUserConfig {
-  const url: string = "https://" + network + ".infura.io/v3/" + infuraApiKey;
-  return {
-    accounts: {
-      count: 10,
-      initialIndex: 0,
-      mnemonic,
-      path: "m/44'/60'/0'/0",
-    },
-    chainId: chainIds[network],
-    url,
-  };
-}
-
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      accounts: {
-        mnemonic,
-      },
-      chainId: chainIds.hardhat,
+      accounts,
+      chainId: 31337,
     },
-    goerli: createTestnetConfig("goerli"),
-    kovan: createTestnetConfig("kovan"),
-    rinkeby: createTestnetConfig("rinkeby"),
-    ropsten: createTestnetConfig("ropsten"),
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 42,
+    },
   },
   namedAccounts: {
     deployer: 0,
