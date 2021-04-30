51be9ca637333bbc757f5e50cd1a8abc3378142d
Yos Riady <yosriady@gmail.com>
2021-04-30 20:57:08 +0800

    Use Unicode for fiat Denominations

--- contracts/libraries/Denominations.sol
+++ contracts/libraries/Denominations.sol
@@ -3,8 +3,12 @@
 pragma solidity ^0.7.0;
 
 library Denominations {
-  address public constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE; // WETH?
-  address public constant USD = 0x0000000000000000000000000000000000000001;
-  address public constant BTC = 0x0000000000000000000000000000000000000002; // WBTC?
+  address public constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+  address public constant BTC = 0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB;
+
+  address public constant USD = 0x0000000000000000000000000000000000000024; // Fiat currencies are identified by the Unicode of their currency symbol
+  address public constant GBP = 0x00000000000000000000000000000000000000A3;
+  address public constant JPY = 0x00000000000000000000000000000000000000A5;
+  address public constant EUR = 0x00000000000000000000000000000000000020Ac;
   // ...
 }
