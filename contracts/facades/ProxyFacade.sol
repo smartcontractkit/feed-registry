97a73fb9213309f7d8dfa829acabcaf493b6fc7d
Yos Riady <yos.riady@smartcontract.com>
2021-05-20 19:59:18 +0800

    Add back ProxyFacade

---
+++ contracts/facades/ProxyFacade.sol
@@ -0,0 +1,193 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.7.6;
+
+import "@chainlink/contracts/src/v0.7/interfaces/AggregatorV2V3Interface.sol";
+import "../interfaces/FeedRegistryInterface.sol";
+
+/**
+  * @notice facade proxy contract that conforms to the AggregatorV2V3Interface.
+  * This contract sits between AggregatorProxy -> ProxyFacade -> FeedRegistry
+  */
+contract ProxyFacade is AggregatorV2V3Interface {
+  FeedRegistryInterface internal immutable s_FeedRegistry;
+  address internal immutable s_asset;
+  address internal immutable s_denomination;
+
+  constructor(
+    address FeedRegistry,
+    address asset,
+    address denomination
+  ) {
+    s_FeedRegistry = FeedRegistryInterface(FeedRegistry);
+    s_asset = asset;
+    s_denomination = denomination;
+  }
+
+  function getFeedRegistry()
+    public
+    view
+    returns (
+      FeedRegistryInterface
+    )
+  {
+    return s_FeedRegistry;
+  }
+
+  function getAsset()
+    public
+    view
+    returns (
+      address
+    )
+  {
+    return s_asset;
+  }
+
+  function getDenomination()
+    public
+    view
+    returns (
+      address
+    )
+  {
+    return s_denomination;
+  }
+
+  // V2
+
+  function latestAnswer()
+    public
+    view
+    virtual
+    override
+    returns
+    (
+      int256
+    )
+  {
+    return s_FeedRegistry.latestAnswer(s_asset, s_denomination);
+  }
+
+  function latestTimestamp()
+    public
+    view
+    virtual
+    override
+    returns (
+      uint256
+    )
+  {
+    return s_FeedRegistry.latestTimestamp(s_asset, s_denomination);
+  }
+
+  function latestRound()
+    public
+    view
+    virtual
+    override
+    returns (
+      uint256
+    )
+  {
+    return s_FeedRegistry.latestRound(s_asset, s_denomination);
+  }
+
+  function getAnswer(
+    uint256 roundId
+  )
+    public
+    view
+    virtual
+    override
+    returns (
+      int256
+    )
+  {
+    return s_FeedRegistry.getAnswer(s_asset, s_denomination, roundId);
+  }
+
+  function getTimestamp(
+    uint256 roundId
+  )
+    public
+    view
+    virtual
+    override
+    returns (
+      uint256
+    )
+  {
+    return s_FeedRegistry.getTimestamp(s_asset, s_denomination, roundId);
+  }
+
+  // V3
+
+  function decimals()
+    public
+    view
+    virtual
+    override
+    returns (
+      uint8
+    )
+  {
+    return s_FeedRegistry.decimals(s_asset, s_denomination);
+  }
+
+  function description()
+    public
+    view
+    virtual
+    override
+    returns (
+      string memory
+    )
+  {
+    return s_FeedRegistry.description(s_asset, s_denomination);
+  }
+
+  function version()
+    public
+    view
+    virtual
+    override
+    returns (
+      uint256
+    )
+  {
+    return s_FeedRegistry.version(s_asset, s_denomination);
+  }
+
+  function getRoundData(uint80 _roundId)
+    public
+    view
+    virtual
+    override
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    )
+  {
+    return s_FeedRegistry.getRoundData(s_asset, s_denomination, _roundId);
+  }
+
+  function latestRoundData()
+    public
+    view
+    virtual
+    override
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    )
+  {
+    return s_FeedRegistry.latestRoundData(s_asset, s_denomination);
+  }
+}
---
+++ test/facades/ProxyFacade.test.ts
@@ -0,0 +1,50 @@
+import hre from "hardhat";
+import { Artifact } from "hardhat/types";
+import { FeedRegistry } from "../../typechain/FeedRegistry";
+import { expect } from "chai";
+import { TEST_ANSWER, ASSET, DENOMINATION } from "../utils/constants";
+import { contract } from "../utils/context";
+import { deployMockAggregator } from "../utils/mocks";
+
+const { deployContract } = hre.waffle;
+
+contract("ProxyFacade", function () {
+  beforeEach(async function () {
+    const FeedRegistryArtifact: Artifact = await hre.artifacts.readArtifact("FeedRegistry");
+    this.registry = <FeedRegistry>await deployContract(this.signers.owner, FeedRegistryArtifact, []);
+
+    this.feed = await deployMockAggregator(this.signers.owner);
+    await this.registry.proposeFeed(ASSET, DENOMINATION, this.feed.address);
+    await this.registry.confirmFeed(ASSET, DENOMINATION, this.feed.address);
+
+    const proxyFacadeArtifact: Artifact = await hre.artifacts.readArtifact("ProxyFacade");
+    this.proxyFacade = await deployContract(this.signers.owner, proxyFacadeArtifact, [
+      this.registry.address,
+      ASSET,
+      DENOMINATION,
+    ]);
+    this.owned = this.proxyFacade;
+
+    const proxyArtifact: Artifact = await hre.artifacts.readArtifact("AggregatorProxy");
+    this.proxy = await deployContract(this.signers.owner, proxyArtifact, [this.proxyFacade.address]);
+  });
+
+  it("proxyFacade should be initialized correctly", async function () {
+    expect(await this.proxyFacade.getFeedRegistry()).to.equal(this.registry.address);
+    expect(await this.proxyFacade.getAsset()).to.equal(ASSET);
+    expect(await this.proxyFacade.getDenomination()).to.equal(DENOMINATION);
+    expect(await this.proxyFacade.latestAnswer()).to.equal(TEST_ANSWER);
+    // TODO: other getters
+  });
+
+  it("proxyFacade should be able to read answer from registry", async function () {
+    expect(await this.proxyFacade.latestAnswer()).to.equal(TEST_ANSWER);
+    // TODO: other getters
+  });
+
+  it("proxy should be able to read answer through proxyFacade", async function () {
+    expect(await this.proxy.aggregator()).to.equal(this.proxyFacade.address);
+    expect(await this.proxy.latestAnswer()).to.equal(TEST_ANSWER);
+    // TODO: other getters
+  });
+});
