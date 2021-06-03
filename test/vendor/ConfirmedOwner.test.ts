46029f76c9820b999d5f7773e00bcd0d7aa41f59
Yos Riady <yos.riady@smartcontract.com>
2021-06-03 23:08:39 +0800

    Use latest ConfirmedOwner, add zero address guard

---
+++ test/vendor/ConfirmedOwner.test.ts
@@ -0,0 +1,28 @@
+import hre, { ethers } from "hardhat";
+import { expect } from "chai";
+import { contract } from "../utils/context";
+import { ConfirmedOwner } from "../../typechain";
+import { Artifact } from "hardhat/types";
+
+const { deployContract } = hre.waffle;
+
+contract("ConfirmedOwner", function () {
+  it("Can deploy with nonzero address as owner", async function () {
+    const confirmedOwnerArtifact: Artifact = await hre.artifacts.readArtifact(
+      "contracts/vendor/ConfirmedOwner.sol:ConfirmedOwner",
+    );
+    this.owned = <ConfirmedOwner>(
+      await deployContract(this.signers.owner, confirmedOwnerArtifact, [this.signers.other.address])
+    );
+    expect(await this.owned.owner()).to.equal(this.signers.other.address);
+  });
+
+  it("Cannot deploy with zero address as owner", async function () {
+    const confirmedOwnerArtifact: Artifact = await hre.artifacts.readArtifact(
+      "contracts/vendor/ConfirmedOwner.sol:ConfirmedOwner",
+    );
+    await expect(
+      deployContract(this.signers.owner, confirmedOwnerArtifact, [ethers.constants.AddressZero]),
+    ).to.be.revertedWith("New owner cannot be zero address");
+  });
+});
